89e07ae8ff66cf4d6ff8f2067c5afd2582d52182
Franco Smit <francoissmt@gmail.com>
2021-06-26 21:29:15 +0200

    Added project and added image, text and contact info.

---
+++ MyLandingPage.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", animate);
+                        
+function animate() {
+    
+    
+    setTimeout(animateOccupation, 5000)
+}
+
+function animateOccupation() {
+    
+}
